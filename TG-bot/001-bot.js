267c6ca22b675695c3a31f49bf52d48d0b1a610e
JIEMO <2569995914@qq.com>
2020-05-15 16:05:04 +0800

    bot init

--- TG-bot/001-bot.js
+++ TG-bot/001-bot.js
@@ -1,20 +1,43 @@
-const TelegramBot = require('node-telegram-bot-api');
+function doPost(e){
+  var estringa = JSON.parse(e.postData.contents);
+  var payload = identificar(estringa);
+  var data = {
+    "method": "post",
+    "payload": payload
+  }
+  UrlFetchApp.fetch("https://api.telegram.org/bot1204445809:AAHfEyrip5DkXBCfh7wgSYbd_x1A6fUq2fA/", data);
+}
 
-const token = '1204445809:AAHfEyrip5DkXBCfh7wgSYbd_x1A6fUq2fA';
-const bot = new TelegramBot(token, {
-  polling: true
-});
-
-
-
-bot.onText(/\/hentai/, function onLoveText(msg) {
-  bot.sendMessage(msg.chat.id, 'Are you a hetai?');
-});
-
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
-});
+function identificar(e){
+  if (e.message.text){
+    var mensaje = {
+      "method": "sendMessage",
+      "chat_id": e.message.chat.id,
+      "text": e.message.text,
+    } 
+  }
+  else if (e.message.sticker){
+    var mensaje = {
+      "method": "sendSticker",
+      "chat_id": e.message.chat.id,
+      "sticker": e.message.sticker.file_id
+    }
+   }
+  else if (e.message.photo){
+    var array = e.message.photo;
+    var text = array[1];
+    var mensaje = {
+      "method": "sendPhoto",
+      "chat_id": e.message.chat.id,
+      "photo": text.file_id
+    }
+   }
+    else {
+    var mensaje = {
+      "method": "sendMessage",
+      "chat_id": e.message.chat.id,
+      "text": "Try other stuff"
+    }
+   }
+  return mensaje
+}
